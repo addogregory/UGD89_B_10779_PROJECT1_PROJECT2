bdad12ca8154317494d03cf19fe29ced1f952adc
addogregory <greg.yodha05@gmail.com>
2022-11-06 20:54:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ugd89_b_10779_project1/MainActivity.kt
@@ -0,0 +1,167 @@
+package com.example.ugd89_b_10779_project1
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Build
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import com.example.ugd89_b_10779_project1.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private var binding: ActivityMainBinding? = null
+    private val CHANNEL_ID_1 = "channel_notification_01"
+    private val CHANNEL_ID_2 = "channel_notification_02"
+    private val notificationId1 = 101
+    private val notificationId2 = 102
+
+    private var lastUpdate: Long = 0
+    private var last_x = 0f
+    private var last_y = 0f
+    private var last_z = 0f
+    private var mSensorManager: SensorManager? = null
+    private var mAccelerometers: Sensor? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        checkPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE)
+        mSensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        mAccelerometers = mSensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        createNotificationChannel()
+    }
+
+
+    fun checkPermission(permission: String, requestCode: Int) {
+        // Checking if permission is not granted
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                permission
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission), requestCode)
+        } else {
+            Toast.makeText(this@MainActivity, "Permission already granted", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this@MainActivity, "Camera Permission Granted", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                Toast.makeText(this@MainActivity, "Camera Permission Denied", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    override fun onSensorChanged(sensorEvent: SensorEvent) {
+        val mySensor = sensorEvent.sensor
+        if (mySensor.type == Sensor.TYPE_ACCELEROMETER) {
+            val x = sensorEvent.values[0]
+            val y = sensorEvent.values[0]
+            val z = sensorEvent.values[0]
+            val curTime = System.currentTimeMillis()
+            if (curTime - lastUpdate > 100) {
+                val diffTime = curTime - lastUpdate
+                lastUpdate = curTime
+                val speed = Math.abs(x + y + y - last_x - last_y - last_z) / diffTime * 10000
+                if (speed > SHAKE_THRESHOLD) {
+                    val intent = Intent(this@MainActivity, CameraActivity::class.java)
+                    startActivity(intent)
+                }
+                last_x = x
+                last_y = y
+                last_z = z
+                sendNotification1()
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, i: Int) {}
+    public override fun onResume() {
+        super.onResume()
+        mSensorManager!!.registerListener(this, mAccelerometers, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        mSensorManager!!.unregisterListener(this)
+    }
+
+    companion object {
+        private const val SHAKE_THRESHOLD = 600
+        private const val CAMERA_PERMISSION_CODE = 100
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Notification Tittle"
+            val descriptionText = "Notification Description"
+
+            val channel1 = NotificationChannel(CHANNEL_ID_1, name, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = descriptionText
+            }
+
+            val channel2 = NotificationChannel(CHANNEL_ID_2, name, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = descriptionText
+            }
+
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel1)
+            notificationManager.createNotificationChannel(channel2)
+        }
+    }
+
+    private fun sendNotification1() {
+
+        val intent : Intent = Intent(this,MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent : PendingIntent = PendingIntent.getActivity(this, 0,intent, PendingIntent.FLAG_MUTABLE)
+
+        val broadcastIntent : Intent = Intent(this, NotificationReceiver::class.java)
+        broadcastIntent.putExtra("toastMessage","Selamat anda sudah berhasil mengerjakan Modul 8 dan 9 ")
+        val actionIntent = PendingIntent.getBroadcast(this,0, broadcastIntent, PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val builder = NotificationCompat.Builder(this,CHANNEL_ID_1)
+            .setSmallIcon(R.drawable.ic_baseline_notifications_24)
+            .setContentTitle("Modul89_B_10798_PROJECT2")
+            .setContentText("Selamat anda sudah berhasil mengerjakan Modul 8 dan 9 ")
+            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+            .setColor(Color.BLUE)
+            .setAutoCancel(true)
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+            .addAction(R.mipmap.ic_launcher_round, "Toast", actionIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)){
+            notify(notificationId1, builder.build())
+        }
+    }
+}
